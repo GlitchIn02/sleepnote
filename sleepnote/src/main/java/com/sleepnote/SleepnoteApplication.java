beb4c63f117a41393a25561b16c16403aaf73bff
lee <lee@DESKTOP-8J34SDV>
2023-06-25 18:24:37 +0900

    create

---
+++ sleepnote/src/main/java/com/sleepnote/SleepnoteApplication.java
@@ -0,0 +1,13 @@
+package com.sleepnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepnoteApplication.class, args);
+	}
+
+}
---
+++ sleepnote/src/test/java/com/sleepnote/SleepnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
